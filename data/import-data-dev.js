51d6cfa5bf2e946636419e942322d3e938214b15
zedxog <zubydev2012@gmail.com>
2022-02-01 22:57:07 +0100

    added build

--- data/import-data-dev.js
+++ data/import-data-dev.js
@@ -8,7 +8,7 @@ dotenv.config({
     path: "./config.env"
 });
 
-mongoose.connect(process.env.DATABASE_LOCAL, {
+mongoose.connect(process.env.DATABASE_ATLAS, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
